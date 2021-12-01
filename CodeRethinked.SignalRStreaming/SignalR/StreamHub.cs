583962b1ec7900e4f7d1098a2fa0ee8e18bb4449
Yunus <yunusozdemir468@gmail.com>
2021-12-02 00:40:37 +0300

    initial commit

---
+++ CodeRethinked.SignalRStreaming/SignalR/StreamHub.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace CodeRethinked.SignalRStreaming.SignalR
+{
+    public class StreamHub : Hub
+    {
+        public ChannelReader<int> DelayCounter(int delay)
+        {
+            var channel = Channel.CreateUnbounded<int>();
+
+            _ = WriteItems(channel.Writer,  delay);
+
+            return channel.Reader;
+        }
+
+        private async Task WriteItems(ChannelWriter<int> writer, int delay)
+        {
+            for (; ; )
+            {
+                //For every 5 items streamed, add twice the delay
+              
+
+                await writer.WriteAsync(100);
+                await Task.Delay(delay);
+            }
+
+            writer.TryComplete();
+        }
+    }
+}
